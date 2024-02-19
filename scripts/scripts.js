646c3d27e4d6ccbd6752c274405029e534cdfac1
sujon157 <riponahamed1579@gmail.com>
2024-02-20 02:13:58 +0600

    My commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,60 @@
 
+let count=0;
+const allBtn=document.getElementsByClassName('kbd');
 
-document.getElementById('seat-container').addEventListener('click',function(event){
-    const targetKbd=event.target;
-    // const expectedKbd=targetKbd.value;
-    console.log(targetKbd);
-    if(targetKbd.kbd==='kbd'){
-        targetKbd.classList.toggle('selected');
-    }
-})
+
+for(const btn of allBtn){
+    btn.addEventListener('click',function(){
+     
+      
+      if(count>=4){
+        return alert('Enough!');
+      }
+      
+      btn.style.backgroundColor='green';
+      count+=1;  
+      const seatUpdated=document.getElementById('seat-update');
+      seatUpdated.innerText=count;
+
+      const seatDecrease=document.getElementById('seat-decrease');
+      seatDecrease.innerText=4-count;
+ 
+    //  const Seat=document.getElementById('seat');
+    //  Seat.innerText=btn.innerText;
+    //  const Class=document.getElementById('class');
+    //  Class.innerText='economy';
+    //  const amount=document.getElementById('amount');
+    //  amount.innerText=650;
+
+      
+      const ticketInfo=document.getElementById('ticket-info');
+      const tr=document.createElement('tr');
+
+      const td1=document.createElement('td');
+      td1.innerText=btn.innerText;
+      tr.appendChild(td1);
+
+      const td2=document.createElement('td');
+      td2.innerText='Economy';
+      tr.appendChild(td2);
+
+      const td3=document.createElement('td');
+      td3.innerText=550;
+      tr.appendChild(td3);
+
+     
+      ticketInfo.appendChild(tr);
+
+    
+
+
+
+
+        
+    })
+}
+
+
+
+
+
